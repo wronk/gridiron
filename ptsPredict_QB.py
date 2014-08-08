9b839a62c1e335498c6121393ad91f94201e36ae
wronk <wronk.mark@gmail.com>
2014-08-07 21:07:38 -0700

    QB stats pull correctly. Began defensive stat query

--- ptsPredict_QB.py
+++ ptsPredict_QB.py
@@ -25,19 +25,19 @@ q.player(years_pro__ge=len(seasons), position=pos)
 
 # GSIS ID number of matching QBs
 qb_id = [p.player_id for p in q.as_players()]
-qb_id = qb_id[0:2]
+#qb_id = qb_id[0:2]
 #######################################
 ### Cull selected stats for all these QBs
-qb_game_ids = []
-qb_game_agg = []
+qb_game_ids = [] # List of lists of game GSIS ID numbers
+qb_game_agg = [] # List of lists of PlayPlayer objects for each game
 
-# get player ID numbers 
+# Get game ID numbers for each player
 for player_id in qb_id:
     q = nfldb.Query(db).game(season_year=seasons, season_type='Regular')
     q.player(player_id=player_id)
     qb_game_ids.append([g.gsis_id for g in q.as_games()])
 
-# get all stats on game by game basis for each player
+# get all stats on a game by game basis for each player
 for player_id, g_list in zip(qb_id, qb_game_ids):
     temp_list=[]
     for g_id in g_list:
@@ -49,15 +49,46 @@ for player_id, g_list in zip(qb_id, qb_game_ids):
     qb_game_agg.append(temp_list)
 
 #######################################
-### Pull selected stats from set of all stats
+### Pull desired offensive stats from set of all stats
 
+qb_select_stats = []
 # find intersection of recorded stats and desired stats
 for player_game_list in qb_game_agg:
-    for game in player_game_list:
+    player_select_stats = np.zeros((len(player_game_list), len(stats_off)))
+    for gi, game in enumerate(player_game_list):
 	for attr in game.fields.intersection(stats_off):
-	    print attr
-
-
-#union = pp.fields.intersection(stats_off)
+	    player_select_stats[gi, stats_off.index(attr)] = getattr(game, attr)
+    qb_select_stats.append(player_select_stats)
 
 #######################################
+### Pull desired defensive stats from set of all stats
+# Get list of defensive player IDs faced by QB for a game
+
+# Get list of plays QB played in
+# Get PlayPlayer objects for each Play
+# Get unique list of defensive players from PlayPlayer object
+#######################################
+def player_def_stats(player_id, team, stats_desired, seasons=[2013],
+		     season_type='Regular'):
+
+    # in: player, team, year, stats desired
+    # out: avg stats over games desired
+    
+    # Get game ID numbers (GSIS_ID) for player of interest
+    game_ids=[]
+    q = nfldb.Query(db).game(season_year=seasons, season_type=season_type)
+    q.player(player_id=player_id)
+    game_ids.append([g.gsis_id for g in q.as_games()])
+
+    # Get all stats on a game by game basis for desired player
+    player_stats_all = []
+    for g_id in game_ids:
+	q = nfldb.Query(db).game(season_year=seasons, gsis_id=g_id)
+	q.player(player_id=player_id)
+	player_stats_all.append(q.as_aggregate()[0])
+
+    # Get all stats on a game by game basis for desired player
+    player_select_stats = np.zeros((len(game_ids), len(stats_desired)))
+    for gi, game in enumerate(player_game_list):
+	for attr in game.fields.intersection(stats_off):
+	    player_select_stats[gi, stats_off.index(attr)] = getattr(game, attr)
